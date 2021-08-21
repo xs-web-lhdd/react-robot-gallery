bd17f42b1561fb25132c1e1fdfff593e24807b9a
凉风有信 <3042146237@qq.com>
2021-08-21 14:22:28 +0800

    feat:css模块化=>jss

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import robots from './mockdata/robots.json'
 import Robot from './components/Robot'
+import styles from './App.module.css'
 
 function App() {
   return (
-    <ul>
-      {robots.map(r => <Robot id={r.id} name={r.name} email={r.email} />)}
-    </ul>
+    <div className={styles.app}>
+      <div className={styles.robotList}>
+        {robots.map(r => <Robot id={r.id} name={r.name} email={r.email} />)}
+      </div>
+    </div>
   );
 }
 
--- src/components/Robot.tsx
+++ src/components/Robot.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from './Robot.module.css'
 
 interface RobotProps {
   id: number,
@@ -7,11 +8,13 @@ interface RobotProps {
 }
 
 const Robot: React.FC<RobotProps> = ({ id, name, email }) => {
-  return <li>
-    <img alt="robot" src={`https://robohash.org/${id}`} />
-    <h2>{name}</h2>
-    <p>{email}</p>
-  </li>
+  return (
+    <div className={styles.cardContainer}>
+      <img alt="robot" src={`https://robohash.org/${id}`} />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  )
 }
 
 export default Robot
